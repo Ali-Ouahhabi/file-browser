76e04a7da797665fa36ecca9a52433708b01fb60
Ali Ouahhabi <ali.ohhb@gmail.com>
2021-02-26 22:11:09 +0100

    proto test lilbit of styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,111 @@
 import './App.css';
+import FileTag from './components/FileTag';
 
 function App() {
+  
+  const tree={
+    parent :null,
+		name:"/root",
+		isFile:false,
+		children:[
+      {
+        name:"Document",
+        isFile:false,
+        children:[
+          {
+            name:"Music",
+            isFile:false,
+            children:[
+              {
+                name:"music1.mp3",
+                isFile:true,
+                children:null
+              } ,
+              {
+                name:"music2.mp3",
+                isFile:true,
+                children:null
+              } ,
+              {
+                name:"music3.mp3",
+                isFile:true,
+                children:null
+              } 
+            ]
+          },
+          {
+            name:"Videos",
+            isFile:false,
+            children:[
+              {
+                name:"vid1.mp4",
+                isFile:true,
+                children:null
+              },
+              {
+                name:"vid1.avi",
+                isFile:true,
+                children:null
+              }  
+            ]
+          },
+          {
+            name:"Pictures",
+            isFile:false,
+            children:[
+              
+            ]
+          },
+          
+        ]
+      },
+      {
+        name:"Download",
+        isFile:false,
+        children:[
+          {
+            name:"Programmes",
+            isFile:false,
+            children:[
+              {
+                name:"VsCode.exe",
+                isFile:true,
+                children:null
+              },
+              {
+                name:"netbeans.exe",
+                isFile:true,
+                children:null
+              }  
+            ]
+          },
+          {
+            name:"Document",
+            isFile:false,
+            children:[
+              {
+                name:"HelloWorld.pdf",
+                isFile:true,
+                children:null
+              },
+              {
+                name:"text.txt",
+                isFile:true,
+                children:null
+              }  
+            ]
+          },
+          
+        ]
+      },
+
+    ],
+
+  }
+
   return (
     <div className="App">
-     <FileTag></FileTag>
+     <FileTag self={tree} level="|"></FileTag>
     </div>
   );
 }
--- src/components/FileTag.js
+++ src/components/FileTag.js
@@ -1,68 +1,88 @@
 import React from "react"
+import "./FileTag.scss"
+import { ImFolder, ImFolderOpen, ImFileText2 } from "react-icons/im";
+
+class FileTag extends React.Component {
+	/**/
+	state = {
+		parent: "",
+		name: "",
+		isFile: "",
+		children: [],
+		index: "",//optional if not by reference
+		isToggeld: false
+	}
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			...this.state,
+			parent: props.parent,
+			...props.self
+		}
+		this.getSelf = this.getSelf.bind(this);
+	}
 
 
-class FileTag extends React.Component{
-/**/
-    state = {
-		parent :"",
-		name:"",
-		isFile:"",
-		children:[],
-		index:"",//optional if not by reference
-		isToggeld:false 
-	}
-	
-	constructor(isFile, name, parent, children){
-		setState((props, state)=>{
-			return{...state, 
-			parent : props.parent,
-			name: props.self.name,
-			isFile: props.self.isfile,
-			children: props.self.children}
-		})
-		this.getSelf= this.getSelf.bind(this);
-	}
-	
-	getSelf(){
+	getSelf() {
 		return this;
 	}
-	
-	onDrop(target){
-		
-		setState((state)=>{
-			st = {...state}
+
+	onDrop(target) {
+
+		this.setState((state) => {
+			let st = { ...state }
 			st.parent.removeChild(st)// here or on drag 
 			st.parent = target.getSelf()
-			target.addChild.push(st).sort()
-			
+			target.addChild.push(st).sort()// ?? sorts
+
 		})
-		
+
 	}
 
-    emit(str,obj){}
-	
-	removeChild(){}
-	
-	getPath(){}
-	
-	sortBy(){}
-	togel(){}
+	emit(str, obj) { }
 
-	render (){
-		return(
-            <div class={props.class} onclick={()=>{setState({isToggeld:!isToggeld}); emit("selected",this)}} >
-                <span className="name">{this.state.name}</span>
-                
-                <div>
-                {
-                this.state.children.map((child)=>{
-                <FileTag class={child.isFile?"file":"folder"} parent = {this} self = {child} />})
-                }
-                </div>
-            </div>
-        )
-		
+	removeChild() { }
+
+	getPath() {
+		return (this.state.parent ? this.state.parent.getPath() + "/" : "") + this.state.name;
 	}
-	
-} 
+
+	sortBy() { }
+	togel() { }
+
+	render() {
+		let children = () => {
+			if (this.props.self.children && this.state.isToggeld) {
+				return this.props.self.children.map((child) => {
+					console.log("child", child);
+					return (
+						<FileTag parent={this} self={child} level={this.props.level + " |"} />
+				)
+				})
+			}
+		};
+		let name = "file-tag-" + (this.state.isFile ? "file" : "folder")
+
+		return ([
+			<div className={name}
+				onClick={(e) => { e.preventDefault(); this.setState({ isToggeld: !this.state.isToggeld }) }}
+				
+			>
+				<span className={"file-tag"+"-icon"}>
+					{this.state.isFile ? <ImFileText2/> : this.state.isToggeld ? <ImFolderOpen/> : <ImFolder/>}
+				 </span>
+				 <span className={"file-tag"+"-name"}> {this.props.self.name}</span>				
+				
+			</div>,
+			<div className={name+"-children"}>
+				{
+					children()
+				}
+			</div>
+		])
+
+	}
+
+}
 export default FileTag;
