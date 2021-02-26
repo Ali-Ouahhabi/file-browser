5c76f063cdd0f1a26ce6739eef5fb207fb99a098
Ali Ouahhabi <ali.ohhb@gmail.com>
2021-02-26 05:01:07 +0100

    Hello World

---
+++ src/components/FileTag.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+class FileTag extends React.Component{
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return <div class="file_">Hello World</div>
+    }
+
+} 
+export default FileTag;
