a8280784d576e797d914d35534f8f434abd9b422
Ali Ouahhabi <ali.ohhb@gmail.com>
2021-02-27 05:03:23 +0100

    adding dragNdrop + remove and onDrop functions cleaning on state

--- src/App.js
+++ src/App.js
@@ -1,101 +1,103 @@
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 import './App.css';
 import FileTag from './components/FileTag';
 
 function App() {
-  
-  const tree={
-    parent :null,
-		name:"/root",
-		isFile:false,
-		children:[
+
+  const tree = {
+    parent: null,
+    name: "root",
+    isFile: false,
+    children: [
       {
-        name:"Document",
-        isFile:false,
-        children:[
+        name: "Document",
+        isFile: false,
+        children: [
           {
-            name:"Music",
-            isFile:false,
-            children:[
+            name: "Music",
+            isFile: false,
+            children: [
               {
-                name:"music1.mp3",
-                isFile:true,
-                children:null
-              } ,
-              {
-                name:"music2.mp3",
-                isFile:true,
-                children:null
-              } ,
-              {
-                name:"music3.mp3",
-                isFile:true,
-                children:null
-              } 
-            ]
-          },
-          {
-            name:"Videos",
-            isFile:false,
-            children:[
-              {
-                name:"vid1.mp4",
-                isFile:true,
-                children:null
+                name: "music1.mp3",
+                isFile: true,
+                children: null
               },
               {
-                name:"vid1.avi",
-                isFile:true,
-                children:null
-              }  
+                name: "music2.mp3",
+                isFile: true,
+                children: null
+              },
+              {
+                name: "music3.mp3",
+                isFile: true,
+                children: null
+              }
             ]
           },
           {
-            name:"Pictures",
-            isFile:false,
-            children:[
-              
+            name: "Videos",
+            isFile: false,
+            children: [
+              {
+                name: "vid1.mp4",
+                isFile: true,
+                children: null
+              },
+              {
+                name: "vid1.avi",
+                isFile: true,
+                children: null
+              }
             ]
           },
-          
+          {
+            name: "Pictures",
+            isFile: false,
+            children: [
+
+            ]
+          },
+
         ]
       },
       {
-        name:"Download",
-        isFile:false,
-        children:[
+        name: "Download",
+        isFile: false,
+        children: [
           {
-            name:"Programmes",
-            isFile:false,
-            children:[
+            name: "Programmes",
+            isFile: false,
+            children: [
               {
-                name:"VsCode.exe",
-                isFile:true,
-                children:null
+                name: "VsCode.exe",
+                isFile: true,
+                children: null
               },
               {
-                name:"netbeans.exe",
-                isFile:true,
-                children:null
-              }  
+                name: "netbeans.exe",
+                isFile: true,
+                children: null
+              }
             ]
           },
           {
-            name:"Document",
-            isFile:false,
-            children:[
+            name: "Document",
+            isFile: false,
+            children: [
               {
-                name:"HelloWorld.pdf",
-                isFile:true,
-                children:null
+                name: "HelloWorld.pdf",
+                isFile: true,
+                children: null
               },
               {
-                name:"text.txt",
-                isFile:true,
-                children:null
-              }  
+                name: "text.txt",
+                isFile: true,
+                children: null
+              }
             ]
           },
-          
+
         ]
       },
 
@@ -104,9 +106,12 @@ function App() {
   }
 
   return (
-    <div className="App">
-     <FileTag self={tree} level="|"></FileTag>
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <div className="App" >
+        <FileTag self={tree} path={"/"+tree.name}></FileTag>
+      </div>
+    </DndProvider>
+
   );
 }
 
--- src/components/FileTag.js
+++ src/components/FileTag.js
@@ -1,48 +1,71 @@
 import React from "react"
 import "./FileTag.scss"
 import { ImFolder, ImFolderOpen, ImFileText2 } from "react-icons/im";
+import { ItemTypes } from "./utli/util";
+import { DragSource, DropTarget } from "react-dnd";
+import { NativeTypes } from "react-dnd-html5-backend";
+
+
+
 
 class FileTag extends React.Component {
 	/**/
 	state = {
-		parent: "",
 		name: "",
 		isFile: "",
-		children: [],
 		index: "",//optional if not by reference
 		isToggeld: false
 	}
+
+
 
 	constructor(props) {
 		super(props);
 		this.state = {
 			...this.state,
 			parent: props.parent,
-			...props.self
+			self: props.self
 		}
 		this.getSelf = this.getSelf.bind(this);
-	}
+		this.onDrop = this.onDrop.bind(this)
 
+	}
 
 	getSelf() {
 		return this;
 	}
 
-	onDrop(target) {
+	onDrop(props, monitor, component) {
 
-		this.setState((state) => {
-			let st = { ...state }
-			st.parent.removeChild(st)// here or on drag 
-			st.parent = target.getSelf()
-			target.addChild.push(st).sort()// ?? sorts
-
-		})
-
+		if (this.state.self.isFile)
+			return this.state.parent.onDrop(props, monitor, component)
+		
+		let item = monitor.getItem()
+		
+		if(item.self == this.state.self || this.props.path.startsWith(item.path))
+			return;
+		let id = item.id;
+		let selfN = {...item.self};
+		let origin = item.parent.getSelf()
+		selfN.parent = props.self
+		this.setState(((state, props) => {
+			let newState = {...state}
+			newState.self.children = state.self.children.concat(selfN)
+			return newState
+			
+		}),()=>origin.removeChild(id))
 	}
 
 	emit(str, obj) { }
 
-	removeChild() { }
+	removeChild(id) {
+		this.setState((state, props) => {
+			let newState = {...state}
+			newState.self.children=state.self.children.slice(0, id).concat(state.self.children.slice(id + 1));
+			return newState
+		})
+
+	}
 
 	getPath() {
 		return (this.state.parent ? this.state.parent.getPath() + "/" : "") + this.state.name;
@@ -53,29 +76,31 @@ class FileTag extends React.Component {
 
 	render() {
 		let children = () => {
-			if (this.props.self.children && this.state.isToggeld) {
-				return this.props.self.children.map((child) => {
-					console.log("child", child);
+			if (this.state.self.children && this.state.isToggeld) {
+				return this.state.self.children.map((child, index) => {
 					return (
-						<FileTag parent={this} self={child} level={this.props.level + " |"} />
-				)
+						<FileTagC key={index+child.name} id={index} parent={this} self={child} path={this.props.path+"/"+child.name}
+						/>
+					)
 				})
 			}
 		};
-		let name = "file-tag-" + (this.state.isFile ? "file" : "folder")
+		let name = "file-tag-" + (this.state.self.isFile ? "file" : "folder")
+
 
 		return ([
-			<div className={name}
-				onClick={(e) => { e.preventDefault(); this.setState({ isToggeld: !this.state.isToggeld }) }}
-				
-			>
-				<span className={"file-tag"+"-icon"}>
-					{this.state.isFile ? <ImFileText2/> : this.state.isToggeld ? <ImFolderOpen/> : <ImFolder/>}
-				 </span>
-				 <span className={"file-tag"+"-name"}> {this.props.self.name}</span>				
-				
-			</div>,
-			<div className={name+"-children"}>
+			this.props.connectDragSource(this.props.connectDropTarget(
+				<div key="head" className={name}
+					onClick={(e) => { e.preventDefault(); this.setState({ isToggeld: !this.state.isToggeld }) }}
+
+				>
+					<span className={"file-tag" + "-icon"}>
+						{this.state.self.isFile ? <ImFileText2 /> : this.state.isToggeld ? <ImFolderOpen /> : <ImFolder />}
+					</span>
+					<span className={"file-tag" + "-name"}> {this.props.self.name}</span>
+
+				</div>)),
+			<div key="body" className={name + "-children"}>
 				{
 					children()
 				}
@@ -85,4 +110,28 @@ class FileTag extends React.Component {
 	}
 
 }
-export default FileTag;
+
+const dropCall = {
+	drop(props, monitor, component) {
+		if (component instanceof FileTag) {
+			component.onDrop(props, monitor, component)
+			return
+		}
+	}
+}
+const dragContent = {
+	beginDrag(props, monitor, component) {
+		return props
+	},
+}
+
+const FileTagC = DragSource('FT', dragContent, (connect, monitor) => ({
+	connectDragSource: connect.dragSource(),
+	isDragging: monitor.isDragging()
+}))(
+	DropTarget('FT', dropCall, connect => ({
+		connectDropTarget: connect.dropTarget(),
+	}))(FileTag)
+);
+
+export default FileTagC;
