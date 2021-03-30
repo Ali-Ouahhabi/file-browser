a27e38151c14da75de3292e6832c312880e51e4e
Ali Ouahhabi <ali.ohhb@gmail.com>
2021-03-30 21:16:55 +0100

    Login form

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.scss';
 import React from 'react';
 import FileManager from './components/file-manager/FileManager/FileManager';
 import Register from "./components/user/register"
+import LogIn from './components/user/login';
 
 function App() {
 
   return (
     <div>
+    <LogIn/>
     <Register></Register>
      
     <FileManager></FileManager> 
---
+++ src/components/user/login.js
@@ -0,0 +1,98 @@
+import React from "react";
+import {
+    Button,
+    Container,
+    Grid,
+    Link,
+    TextField,
+} from "@material-ui/core";
+
+class LogIn extends React.Component {
+    constructor(props){
+        super(props);
+         this.handleChange = this.handleChange.bind(this);
+         this.handleSubmit = this.handleSubmit.bind(this);
+         this.state = {
+         email:"",
+         password:""
+         }
+   }
+ 
+     handleChange(event){
+         console.log("***********************************")
+         console.log(event)
+         this.setState({
+         [event.target.name]: event.target.value
+         });
+     }
+   
+     handleSubmit(){
+         console.log("vals ",this.state);
+     }
+
+     render(){
+         return(
+            <Container component="main" maxWidth="xs">
+            <div>
+                <h1> Log In </h1>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <TextField
+                            variant="outlined"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            onChange={this.handleChange}
+                            value={this.state.email}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            variant="outlined"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                            onChange={this.handleChange}
+                            value={this.state.password}
+                        />
+                    </Grid>
+                </Grid>
+                <Button
+                    style={{
+                        margin: "15px 0",
+                        backgroundColor: "#001e35",
+                        color: "white",
+                        '&:hover': {
+                            backgroundColor: "white",
+                            color: "#001e35",
+                        }
+                    }}
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="#001e35"
+                    onClick={this.handleSubmit}
+                >
+                    Log In
+                </Button>
+                <Grid container justify="flex-end">
+                    <Grid item>
+                        <Link href="#" variant="body2" onClick={this.props.login}>
+                            Don't have an account? Sign in
+                         </Link>
+                    </Grid>
+                </Grid>
+            </div>
+        </Container>
+         )
+     }
+}
+
+export default LogIn;
